b5206542bceab2123f79946dfb46ed41b2121bdd
andy7560683 <34652047+andy7560683@users.noreply.github.com>
2023-10-23 11:44:33 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int prime = 1;
+    for(int i = 2; i <= n-1; i++){
+        if((n % i) == 0){
+           prime = 0;
+        }
+    }
+    if(prime == 1)
+        printf("Yes\n");
+    else
+        printf("No\n");
+    return 0;
+}
 
