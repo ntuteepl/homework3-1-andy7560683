0d205d07cf54425ddc237c753113f1ef049c2658
andy7560683 <34652047+andy7560683@users.noreply.github.com>
2023-10-23 23:55:32 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    int k = 1;
-    for(int i = 2; i < n && i%n!=0; i++){
-        if((n % i) == 0){
-           k = 0;
+int main() {
+    int s1, d1, s2, d2, s3, d3;
+    scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
+    int a[25] = {0};
+    for (int i = s1; i < d1; i++) {
+        a[i]++;
+    }
+    for (int i = s2; i < d2; i++) {
+        a[i]++;
+    }
+    for (int i = s3; i < d3; i++) {
+        a[i]++;
+    }
+    int ans = 0;
+    for (int i = 0; i < 25; i++) {
+        if (a[i] > ans) {
+            ans = a[i];
         }
     }
-    if(k == 1)
-        printf("YES");
-    else
-        printf("NO");
+    printf("%d\n", ans);
     return 0;
 }
